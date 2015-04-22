484ed65351f36079673281585dd0d7e22d0b7407
Micah Eschbacher <michael-e@superbrightleds.com>
2015-04-22 08:59:14 -0500

    Declaring the tables_used property in the trait was causing issues in classes which use it

--- tests/EloquentFunctionalityTrait.php
+++ tests/EloquentFunctionalityTrait.php
@@ -5,8 +5,6 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 trait EloquentFunctionalityTrait 
 {
-    protected $tables_used = [];
-
     /**
      * Begin a new database transaction.
      *
@@ -30,12 +28,20 @@ trait EloquentFunctionalityTrait
 
     protected function clearTables()
     {
+        foreach ($this->sanitizeTablesUsed() as $table) {
+            $this->capsule->table((string)$table)->truncate();
+        }
+    }
+
+    protected function sanitizeTablesUsed() 
+    {
+        if (empty($this->tables_used)) {
+            return []; 
+        }    
         if (!is_array($this->tables_used)) {
-            $this->tables_used = [$this->tables_used];
+            return [$this->tables_used];
         }
 
-        foreach ($this->tables_used as $table) {
-            $this->capsule->table($table)->truncate();
-        }
+        return [];
     }
 }
