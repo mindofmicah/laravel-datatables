c563779c87986e80bf5e7d52a3e705270bacb252
Micah Eschbacher <michael-e@superbrightleds.com>
2015-04-22 09:00:14 -0500

    show off my new request object

--- public/apples.php
+++ public/apples.php
@@ -2,5 +2,9 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 buildCapsuleFromConfigFile(__DIR__ . '/../integrated.json');
+$request = buildRequestObject();
 
+
+echo '<pre>';
+var_dump($request);
 Model::create(['hi'=>'b']);
