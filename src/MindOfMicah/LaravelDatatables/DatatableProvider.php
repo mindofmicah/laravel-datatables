5b886dabdbf42046a20e30fd398d8471ed5d2287
Micah Eschbacher <michael-e@superbrightleds.com>
2015-04-27 15:31:20 -0500

    Typehint the parameter

--- src/MindOfMicah/LaravelDatatables/DatatableProvider.php
+++ src/MindOfMicah/LaravelDatatables/DatatableProvider.php
@@ -2,16 +2,17 @@
 namespace MindOfMicah\LaravelDatatables;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
 
 class DatatableProvider extends ServiceProvider
 {
 	public function register()
 	{
-		\App::bind('MindOfMicah\LaravelDatatables\Datatable', function ($a) {
+		\App::bind('MindOfMicah\LaravelDatatables\Datatable', function (Application $a) {
 			return new \MindOfMicah\LaravelDatatables\Datatable($a->make('request'));
 		});
 		
-		\App::bind('mindofmicah.datatables', function ($a) {
+		\App::bind('mindofmicah.datatables', function (Application $a) {
 			return new \MindOfMicah\LaravelDatatables\Datatable($a->make('request'));
 		});
 	}
