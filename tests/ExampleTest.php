07a2d19eb5f606f8ff4dbf914fc7a08f44e1a0df
Micah Eschbacher <michael-e@superbrightleds.com>
2015-04-28 14:12:46 -0500

    Allow the urls to be generated with a cleaner api

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,6 +2,9 @@
 use Laracasts\TestDummy\Factory as TestDummy;
 use Laracasts\Integrated\Extensions\Traits\ApiRequests;
 
+use MindOfMicah\LaravelDatatables\DatatableQuery\Datatable;
+use MindOfMicah\LaravelDatatables\DatatableQuery\Column;
+
 class ExampleTest extends TestCase
 {
     use EloquentFunctionalityTrait;
@@ -25,10 +28,14 @@ class ExampleTest extends TestCase
             ->seeJSONContains(($a));
 	}
 
-    protected function visitDatatable($key)
+    protected function visitDatatable($key, $dt = null)
     {
-        $url = 'sEcho=2&iColumns=5&sColumns=&iDisplayStart=0&iDisplayLength=-1&mDataProp_0=partNumber&mDataProp_1=stock&mDataProp_2=backorder_qty&mDataProp_3=on_order&mDataProp_4=function&sSearch=&bRegex=false&sSearch_0=&bRegex_0=false&bSearchable_0=true&sSearch_1=&bRegex_1=false&bSearchable_1=true&sSearch_2=&bRegex_2=false&bSearchable_2=true&sSearch_3=&bRegex_3=false&bSearchable_3=true&sSearch_4=&bRegex_4=false&bSearchable_4=false&iSortCol_0=1&sSortDir_0=desc&iSortingCols=1&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=true&bSortable_4=false&_=1429910529659';
-        return $this->visit('/router.php?action=' . $key . '&' . $url);
+        if (!$dt) {
+            $dt = new Datatable;
+            $dt->addDummyColumns(4);
+        }
+
+        return $this->visit('/router.php?action=' . $key . '&' . (string)$dt);
     }
     public function testSelectAllForAGivenModel()
     {
